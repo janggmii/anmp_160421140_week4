9f8b283cf6a4cfe7428753440b9467f09d7cd465
janggmii <janggmii17@gmail.com>
2024-09-20 15:32:57 +0700

    anmp

---
+++ app/src/main/java/com/ubaya/week4_anmp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ubaya.week4_anmp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ubaya.week4_anmp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
